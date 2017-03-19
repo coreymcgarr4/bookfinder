f3118f241b773a8d54f916b617dd638aaf7bdd2b
Corey McGarr <coreymcgarr4@gmail.com>
2017-03-18 20:01:04 -0600

    updates

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,28 +1,14 @@
 angular.module('myApp')
 .controller('mainCtrl', function($scope, mainService){
 
-    $scope.thisAppIsBroken = "This angular app is working";
-//    $scope.searchedBook = mainService.getBook();
+  $scope.getBook = function(search){
+     var promise = mainService.getBook(search);
+     console.log(search);
 
-    // $scope.getBook = function(){
-    //   var data = mainService.getBook();
-    //
-    //   data.then(function(data){
-    //     $scope.data = data;
-    //     console.log(data);
-    //   })
-    // }
-    //
-    // $scope.results = mainService.getBook();
-    //
-    // mainService.getBook().then(function(data){
-    //   $scope.results = data.data.data;
-    // })
-
-    $scope.bookResult = function(){
-      mainService.getBook().then(function(data){
-        $scope.results = data;
-      });
-    }
+     promise.then(function(results){
+         console.log(results);
+         $scope.book = results.data.items;
+       });
+   }
 
 });
--- js/mainService.js
+++ js/mainService.js
@@ -1,13 +1,11 @@
 angular.module('myApp')
 .service('mainService', function($http){
 
-  this.getBook = function(){
-
+  this.getBook = function(search){
+    console.log(search);
     return $http({
       method: 'GET',
-      url: 'https://www.googleapis.com/books/v1/volumes?q=wheel+of+time' //+ 'key=AIzaSyA94ZeIB-pkII-ZrynwLYqquyfOqF-XO4c' //+ "&fields=items(title,author)"
-    }).then(function(response){
-      return response.data;
+      url: 'https://www.googleapis.com/books/v1/volumes?q=' + search
     });
   };
 });
